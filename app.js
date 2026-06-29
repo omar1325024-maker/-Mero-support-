1cdbf3f8e45fc33044e78daec11d63f3dc413371
omar mohamed elnasser adel <omar.1325024@stemdakahlia.moe.edu.eg>
2026-06-29 12:04:18 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,29 @@
-// check login
-if (localStorage.getItem("loggedIn") !== "true") {
+if (!localStorage.getItem("loggedIn")) {
     window.location.href = "login.html";
 }
 
 const posts = [
-    { name:"Omar", title:"Summer Program", goal:1200, raised:560, likes:124 },
-    { name:"Sara", title:"IELTS Fee Support", goal:200, raised:80, likes:45 },
-    { name:"Ahmed", title:"SAT Registration", goal:120, raised:60, likes:78 }
+    {
+        name: "Omar",
+        title: "Accepted to Summer Program",
+        goal: 1200,
+        raised: 560,
+        likes: 124
+    },
+    {
+        name: "Sara",
+        title: "Need IELTS Fee Support",
+        goal: 200,
+        raised: 80,
+        likes: 45
+    },
+    {
+        name: "Ahmed",
+        title: "SAT Registration Support",
+        goal: 120,
+        raised: 60,
+        likes: 78
+    }
 ];
 
 const feed = document.getElementById("feed");
@@ -15,12 +32,19 @@ function render() {
     feed.innerHTML = "";
 
     posts.forEach((p, i) => {
+
+        let percent = (p.raised / p.goal) * 100;
+
         feed.innerHTML += `
         <div class="card">
             <h3>${p.name} 🇪🇬</h3>
             <p>${p.title}</p>
 
-            <p>$${p.raised} / $${p.goal}</p>
+            <div style="background:#eee;height:10px;border-radius:10px;overflow:hidden;">
+                <div style="width:${percent}%;height:10px;background:#4a67ff;"></div>
+            </div>
+
+            <p><b>$${p.raised}</b> / $${p.goal}</p>
 
             <button onclick="like(${i})">❤️ ${p.likes}</button>
             <button onclick="support(${i})">🤝 Support</button>
